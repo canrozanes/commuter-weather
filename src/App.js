6f5b44d27b1cb2f960019e7aa32e09a078022d9e
Can Rozanes <canrozanes@gmail.com>
2019-05-30 17:09:47 -0400

    safi miracle

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			time1: '10:00',
-			time2: '10:00',
+			time1: '',
+			time2: '',
 			// time: '10:00',
 			currently: {},
 			minutely: {},
@@ -19,62 +19,78 @@ class App extends Component {
 		}
 	}
 	handleChange1 = (time) => {
-		this.setState({ time1: time })}
+		console.log(time);
+		
+
+		
+		this.setState({ time1: time })
+	}
+
 	handleChange2 = (time) => {
 		this.setState({ time2: time })
 	}
+	kickOff = (e) => {
+		e.preventDefault();
+		this.formatDate(this.state.time1, "firstWeather");
+		this.formatDate(this.state.time2, "secondWeather");
+	}
 
-
-		// this.setState({
-
-		// })
-		
-	componentDidMount(){
-
-		// axios({
-		// 	url: `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c369d6cf3852faec4a3db6128422f86a/43.6532,-79.3832`,
-		// }).then((response) => {
-		// 	response = response.data
-		// 	this.setState({
-		// 		weather: response
-		// 	})
-		// 	console.log(this.state.weather)
-
-		// })
-		const getWeatherData = () =>{
-			return axios({
-				url: `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c369d6cf3852faec4a3db6128422f86a/43.6532,-79.3832?units=si`,
-				
-			})
+	formatDate = (dateThingy, weatherName) => {
+		let year = dateThingy.getFullYear();
+		let month = dateThingy.getMonth() + 1;
+		if (month <10){
+			month = '0' + month;
 		}
-
-		const getAllData = async () => {
-			const response = await getWeatherData();
-			const weatherData = response.data;
-			// console.log(weatherData)
-			this.setState({
-				currently: weatherData.currently,
-				minutely: weatherData.minutely,
-				hourly: weatherData.hourly
-			})
-			console.log(weatherData)
-			console.log("currently", this.state.currently)
-			console.log("minutely", this.state.minutely)
-			console.log("hourly", this.state.hourly)
+		let day = dateThingy.getDate();
+		if (day < 10) {
+			day = '0' + day;
 		}
-		// getAllData();
-		// console.log(this.state.time)
+		let hours = dateThingy.getHours();
+		if (hours < 10) {
+			hours = '0' + hours;
+		}
+		let minutes = dateThingy.getMinutes();
+		if (minutes < 10) {
+			minutes = '0' + minutes;
+		}
+		let dateString = `${year}-${month}-${day}T${hours}:${minutes}:00`;
+		console.log(dateString);
+		this.getWeatherData(dateString, weatherName);
 
 
 	}
+	getWeatherData = (date, weatherName) => {
+		axios({
+			url: `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c369d6cf3852faec4a3db6128422f86a/43.6532,-79.3832,${date}?units=si`,
+
+		}).then((response)=>{
+			console.log(response);
+			this.setState({
+				[weatherName]: response.data.currently
+			})
+		})
+	}
+	formatToUTC = (date) => {
+		let toUTC = new Date(date).toUTCString()
+		return toUTC;
+	}
+
+	offsetGMTtoToronto = (time) => {
+		return time -14400;
+	}
+		
+		// we do the filtering of the data and set state to contain a 
+		// {weateherIcon1: }
+
 
 	render(){
 		return (
 			<div className="App" >
-				<Header time={this.state} handleChange1={this.handleChange1} handleChange2={this.handleChange2}  />
-
-
-				
+				<Header time1={this.state.time1} 
+						time2={this.state.time2} 
+						handleChange1={this.handleChange1} 
+						handleChange2={this.handleChange2}
+						kickOff={this.kickOff}  />
 				<Footer />
 			</div>
 		);
--- src/Header.js
+++ src/Header.js
@@ -1,32 +1,28 @@
 import React, { Component } from 'react';
-import TimePicker from 'react-time-picker';
+import DateTimePicker from 'react-datetime-picker';
 
 class Header extends Component{
 
     render(){
-        console.log(this.props.time.time1)
         return (
             <div className="header wrapper">
                 <div>
                     <h1>Commuter.Weather</h1>
                     <form action="">
                         <label htmlFor="time1">Commute From Home Start Time</label>
-                        <TimePicker
+                        <DateTimePicker
                             onChange={this.props.handleChange1}
-                            value={this.props.time.time1}
-                            format="HH:mm:a"
-                            name="time1"
-                            disableClock={true}
-                        />
+                            value={this.props.time1}
+                        
+                    />
                         <label htmlFor="time2">Commute From Home Start Time</label>
-                        <TimePicker
+                        <DateTimePicker
                             onChange={this.props.handleChange2}
-                            value={this.props.time.time2}
-                            format="HH:mm:a"
-                            name="time2"
-                            disableClock={true}
+                            value={this.props.time2}
                         />
-                        <button>Get Weather Data</button>
+                        <button onClick={this.props.kickOff}
+                        >Display Weather Data
+                        </button>
                     </form>
                 </div>
             </div>
