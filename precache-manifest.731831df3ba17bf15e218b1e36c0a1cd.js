b49373c10ebde6c261232637106b563756047b5e
Can Rozanes <canrozanes@gmail.com>
2019-06-22 14:05:55 -0400

    Updates

---
+++ precache-manifest.731831df3ba17bf15e218b1e36c0a1cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a252408483fdf9352e6c02f7062c128d",
+    "url": "/canRozanesProject5/index.html"
+  },
+  {
+    "revision": "124cbb1ccf9229bc96e3",
+    "url": "/canRozanesProject5/static/css/2.f68bb24c.chunk.css"
+  },
+  {
+    "revision": "a85a96e084a61113893f",
+    "url": "/canRozanesProject5/static/css/main.60fc273d.chunk.css"
+  },
+  {
+    "revision": "124cbb1ccf9229bc96e3",
+    "url": "/canRozanesProject5/static/js/2.9e478574.chunk.js"
+  },
+  {
+    "revision": "a85a96e084a61113893f",
+    "url": "/canRozanesProject5/static/js/main.6c49db0d.chunk.js"
+  },
+  {
+    "revision": "9294629509ea12d41875",
+    "url": "/canRozanesProject5/static/js/runtime~main.1ecfe060.js"
+  },
+  {
+    "revision": "aba40575e42297f3c2621df1f00faa96",
+    "url": "/canRozanesProject5/static/media/bicycleBackground.aba40575.png"
+  }
+]);
